0134b02d1c14abf0d4431715323ece0806b8f559
Eugene Zheltov <jeez1997@gmail.com>
2019-12-17 13:40:39 +0400

    Started

---
+++ Task5/Logger.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Logger
+    {
+        FileSystemWatcher watcher;
+        object obj = new object();
+        bool enabled = true;
+
+        public Logger()
+        {
+            watcher = new FileSystemWatcher(@"E:\Studying\EPAM_Task5_Files", "*txt");
+            watcher.Deleted += WatcherDeleted;
+            watcher.Created += WatcherCreated;
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+            while (enabled)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+            enabled = false;
+        }
+
+        private void WatcherDeleted(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "deleted";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void WatcherCreated(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "created";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void RecordEntry(string fileEvent, string filePath)
+        {
+            lock (obj)
+            {
+                using (StreamWriter writer = new StreamWriter(@"E:\Studying\EPAM_Task5_logs\log.txt", true)) //стоит убрать writer и сделать копирование файлов?
+                {
+                    writer.WriteLine(String.Format("{0} file {1} was {2}",
+                        DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+                    writer.Flush();
+                }
+            }
+        }
+
+
+    }
+}
--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NLog.Fluent;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,65 @@ namespace Task5
     {
         static void Main(string[] args)
         {
+            int input;
+
+            Console.WriteLine("Which mode do you want to use?");
+            Console.WriteLine("1. Observer");
+            Console.WriteLine("2. Backup");
+
+            while(!(Int32.TryParse(Console.ReadLine(), out input) && (input == 1 || input == 2)))
+            {
+                Console.WriteLine("Incorrect input");
+                Console.WriteLine("Try again");
+            }
+
+            
+            if (input == 1)
+            {
+                DateTime date = DateTime.Now;
+                string logFoler = @"E:\Studying\EPAM_Task5_logs\" + date.Day + "." 
+                    + date.Month + "." + date.Year + "_" + date.Hour + "h" 
+                    + date.Minute + "m" + date.Second + "s";
+                
+                Directory.CreateDirectory(logFoler);
+
+                Logger logger = new Logger();
+            }
+            else if (input == 2)
+            {
+                Console.WriteLine("What time do you back up?");
+                Console.WriteLine("Your input must be in format day.month.year hours:minutes:seconds");
+
+                if (DateTime.TryParse(Console.ReadLine(), out DateTime Time))
+                { 
+                    DirectoryInfo directory = new DirectoryInfo(@"E:\Studying\EPAM_Task5_logs\" + Time.Day + "."
+                    + Time.Month + "." + Time.Year + "_" + Time.Hour + "h"
+                    + Time.Minute + "m" + Time.Second + "s");
+                    
+                    if (directory.Exists)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no backup for this time");                      
+                    }
+                }
+            }
+
+            Console.Read();
+
+
+            //using(var stream = new FileStream(@"E:\Studying\EPAM_Task5_Files", FileMode.Append))
+
+            //using(FileSystemWatcher watcher = new FileSystemWatcher(@"E:\Studying\EPAM_Task5_Files", "*txt"))
+            //{
+            //    watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.CreationTime;
+
+
+            //}
+
+
         }
     }
 }
