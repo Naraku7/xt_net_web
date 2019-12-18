7c4edfea4e1e7eb715f041f9e29073eb5b8f4667
Eugene Zheltov <jeez1997@gmail.com>
2019-12-19 01:26:01 +0400

    Copying files from log folder is fine now. Logging is undone

--- Task5/Logger.cs
+++ Task5/Logger.cs
@@ -15,10 +15,13 @@ namespace Task5
         FileSystemWatcher watcher;
         object obj = new object();
         bool enabled = true;
+        string sourceDir;
+        string logDir = @"E:\Studying\EPAM_Task5_logs\";
 
         public Logger()
         {
-            watcher = new FileSystemWatcher(@"E:\Studying\EPAM_Task5_Files", "*txt");
+            sourceDir = @"E:\Studying\EPAM_Task5_Files";
+            watcher = new FileSystemWatcher(sourceDir, "*txt");
             watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.CreationTime; //нужно ли?
 
             // Add event handlers.
@@ -45,38 +48,109 @@ namespace Task5
 
         private void OnDeleted(object sender, FileSystemEventArgs e)
         {
-            string fileEvent = "deleted";
-            string filePath = e.FullPath;
-            RecordEntry(fileEvent, filePath);
+            //string fileEvent = "deleted";
+            //string filePath = e.FullPath;
+            //File.Delete(filePath);
+            //RecordEntry(fileEvent, filePath);
+            //DirectoryCopy(sourceDir, logDir, true);
+
+            Thread th = new Thread(() =>
+            {
+                DirectoryCopy(sourceDir, logDir, true);
+            });
+
+            th.Start();
+
+            th.Abort();
+
         }
 
         private void OnCreated(object sender, FileSystemEventArgs e)
         {
-            string fileEvent = "created";
-            string filePath = e.FullPath;
-            RecordEntry(fileEvent, filePath);
+            //string fileEvent = "created";
+            //string filePath = e.FullPath;
+            //RecordEntry(fileEvent, filePath);
+            //DirectoryCopy(sourceDir, logDir, true);
+
+            Thread th = new Thread(() =>
+           {
+               DirectoryCopy(sourceDir, logDir, true);
+           });
+
+            th.Start();
+            
+            th.Abort(); //нужно ли?
+            
         }
 
         private void OnChanged(object sender, FileSystemEventArgs e)
         {
-            string fileEvent = "created";
-            string filePath = e.FullPath;
-            RecordEntry(fileEvent, filePath);
+            //string fileEvent = "created";
+            //string filePath = e.FullPath;
+            //RecordEntry(fileEvent, filePath);
+            //DirectoryCopy(sourceDir, logDir, true);
+
+            Thread th = new Thread(() =>
+            {
+                DirectoryCopy(sourceDir, logDir, true);
+            });
+
+            th.Start();
+
+            th.Abort(); 
         }
 
-        private void RecordEntry(string fileEvent, string filePath)
-        {
-            lock (obj)
-            {
-                //по идее тут нужен FSW? Или еще где-то?
-                using (StreamWriter writer = new StreamWriter(@"E:\Studying\EPAM_Task5_logs\log.txt", true)) //стоит убрать writer и сделать копирование файлов?
-                {
-                    
-                    
+        //private void RecordEntry(string fileEvent, string filePath)
+        //{
+        //    lock (obj)
+        //    {
 
-                    //writer.WriteLine(String.Format("{0} file {1} was {2}",
-                    //    DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
-                    //writer.Flush();
+
+        //        ////по идее тут нужен FSW? Или еще где-то?
+        //        //using (StreamWriter writer = new StreamWriter(@"E:\Studying\EPAM_Task5_logs\log.txt", true)) //стоит убрать writer и сделать копирование файлов?
+        //        //{
+        //        //    //writer.WriteLine(String.Format("{0} file {1} was {2}",
+        //        //    //    DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+        //        //    //writer.Flush();
+        //        //}
+        //    }
+        //}
+
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                // Overwrite the files in the destination directory 
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
                 }
             }
         }
--- Task5/Program.cs
+++ Task5/Program.cs
@@ -43,11 +43,8 @@ namespace Task5
                 Console.WriteLine("What time do you back up?");
                 Console.WriteLine("Your input must be in format day.month.year hours:minutes:seconds");
 
-                if (DateTime.TryParse(Console.ReadLine(), out DateTime Time))
-                { 
-                    DirectoryInfo logDirectory = new DirectoryInfo(@"E:\Studying\EPAM_Task5_logs\" + Time.Day + "."
-                    + Time.Month + "." + Time.Year + "_" + Time.Hour + "h"
-                    + Time.Minute + "m" + Time.Second + "s");
+                    DirectoryInfo logDirectory = new DirectoryInfo(@"E:\Studying\EPAM_Task5_logs\" + Console.ReadLine());
+                
 
                     //string logDir = @"E:\Studying\EPAM_Task5_logs\";
                     string filesDir = @"E:\Studying\EPAM_Task5_Files\";
@@ -76,7 +73,7 @@ namespace Task5
                             {
                                 dir.Delete(true);
                             }
-                            catch (System.IO.IOException e)
+                            catch (IOException e)
                             {
                                 Console.WriteLine(e.Message);
                             }
@@ -85,39 +82,37 @@ namespace Task5
                         string fileName; 
                         string destFile;
 
-                        if (Directory.Exists(logDirectory.ToString()))
-                        {
-                            string[] files = Directory.GetFiles(logDirectory.ToString(), "*.txt");
+                        //по идее, Logger.DirectoryCopy() копирует и файлы тоже
 
-                            // Copy the files and overwrite destination files if they already exist.
-                            foreach (string s in files)
-                            {
-                                // Use static Path methods to extract only the file name from the path.
-                                fileName = Path.GetFileName(s);
-                                destFile = Path.Combine(filesDir, fileName);
-                               File.Copy(s, destFile, true);
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Source path does not exist!");
-                        }
+                        //if (Directory.Exists(logDirectory.ToString()))
+                        //{
+                        //    string[] files = Directory.GetFiles(logDirectory.ToString(), "*.txt");
 
-                        
-                        DirectoryCopy(@"E:\Studying\EPAM_Task5_logs\", @"E:\Studying\EPAM_Task5_Files\", true);
+                        //    // Copy the files and overwrite destination files if they already exist.
+                        //    foreach (string s in files)
+                        //    {
+                        //        // Use static Path methods to extract only the file name from the path.
+                        //        fileName = Path.GetFileName(s);
+                        //        destFile = Path.Combine(filesDir, fileName);
+                        //       File.Copy(s, destFile, true);
+                        //    }
+                        //}
+                        //else
+                        //{
+                        //    Console.WriteLine("Source path does not exist!");
+                        //}
+
+
+                        Logger.DirectoryCopy(logDirectory.ToString(), @"E:\Studying\EPAM_Task5_Files\", true);
                     }
-                    else
+                    else //это почему-то никогда не срабатывает
                     {
                         Console.WriteLine("There is no backup for this time");                      
-                    }
-                }
+                    }        
             }
 
             Console.Read();
-
-
-            //using(var stream = new FileStream(@"E:\Studying\EPAM_Task5_Files", FileMode.Append))
-
+      
             //using(FileSystemWatcher watcher = new FileSystemWatcher(@"E:\Studying\EPAM_Task5_Files", "*txt"))
             //{
             //    watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.CreationTime;
@@ -128,43 +123,7 @@ namespace Task5
 
         }
 
-        //изучи этот метод
-        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
-        {
-            // Get the subdirectories for the specified directory.
-            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
-
-            if (!dir.Exists)
-            {
-                throw new DirectoryNotFoundException(
-                    "Source directory does not exist or could not be found: "
-                    + sourceDirName);
-            }
-
-            DirectoryInfo[] dirs = dir.GetDirectories();
-            // If the destination directory doesn't exist, create it.
-            if (!Directory.Exists(destDirName))
-            {
-                Directory.CreateDirectory(destDirName);
-            }
-
-            // Get the files in the directory and copy them to the new location.
-            FileInfo[] files = dir.GetFiles();
-            foreach (FileInfo file in files)
-            {
-                string temppath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(temppath, false);
-            }
-
-            // If copying subdirectories, copy them and their contents to new location.
-            if (copySubDirs)
-            {
-                foreach (DirectoryInfo subdir in dirs)
-                {
-                    string temppath = Path.Combine(destDirName, subdir.Name);
-                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
-                }
-            }
-        }
+        
+       
     }
 }
