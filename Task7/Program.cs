55effa41b3edb897899b062dae44903a0a7e7251
Eugene Zheltov <jeez1997@gmail.com>
2020-01-08 22:18:09 +0400

    All done

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -13,9 +13,13 @@ namespace Task7
         {
             //Console.WriteLine(DateExistence("2016 год наступит 01-12-2020"));
 
-            //TimeCounter("В 7:55 я встал, позавтракал и к 10:77 пошёл на работу.");
+            TimeCounter("В 7:55 я встал, позавтракал и к 77:00 пошёл на работу.");
 
             //Console.WriteLine(HTMLReplacer("<b>Это</b> текст <i>с</i> <font color=\"red\">HTML</font> кодами"));
+
+            //NumberValidator(); 
+
+            //EmailFinder();
 
             Console.ReadLine();
         }
@@ -24,19 +28,79 @@ namespace Task7
 
         public static void TimeCounter(string text)
         {
-            Regex regex = new Regex(@"([0-9]|[10-24]):([0-60])"); //24:07 и подобное тоже считает
+            Regex regex = new Regex(@"(\D[0-9]:[0-5][0-9])|(\D[1][0-9]:[0-5][0-9])|(\D[2][0-3]:[0-5][0-9])");
 
             MatchCollection matches = regex.Matches(text);
 
             Console.WriteLine($"Время в тексте присутствует {matches.Count} раз.");
         }
 
-        //убирает и все, что между
+        
         public static string HTMLReplacer(string text)
-        {
-            Regex regex = new Regex(@"((<\s*a[^>]*>)|(<\s*/\s*a>))|((<\s*b>)|(</\s*b>))|((<\s*i>)|(</\s*i>))|((<\s*font[^>]*>)|(<\s*/\s*font>))");
+        {    
+            Regex regex = new Regex(@"(<\s*[^>]*>)|(<\s*/\s*>)");
 
             return regex.Replace(text, "_");
+        }
+
+        public static void NumberValidator()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите число: ");
+
+                string text = Console.ReadLine();
+
+                if (Regex.IsMatch(text, @"\d"))
+                {
+                    if (Regex.IsMatch(text, "[^0-9]"))
+                    {
+                        if (Regex.IsMatch(text, "[^e\\-0-9\\.]"))
+                        {
+                            Console.WriteLine("Это не число");
+                        }
+                        else if (Regex.IsMatch(text, "e"))
+                        {
+                            Console.WriteLine("Это число в научной нотации");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Это число в обычной нотации");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Это число в обычной нотации");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Это не число");
+                }
+            }
+
+        }
+
+        public static void EmailFinder()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите строку: ");
+
+                string text = Console.ReadLine();
+
+                Regex regex = new Regex(@"(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))");
+
+                Console.WriteLine("Найденные адреса электронной почты:");
+
+                MatchCollection matches = regex.Matches(text);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+            }
         }
     }
 }
